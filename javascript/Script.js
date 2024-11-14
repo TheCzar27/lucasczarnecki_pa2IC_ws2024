551150d5ec799ead0909df876225f52363ce972e
TheCzar27 <Lucasczar27@gmail.com>
2024-11-14 18:56:08 -0500

    CSS rework

--- javascript/Script.js
+++ javascript/Script.js
@@ -1,37 +1,34 @@
-function showFeature (letter) {
+function showFeature(letter) {
   const displayArea = document.getElementById("display-area");
   let content = "";
 
   switch (letter) {
     case "A":
       content = `
-                <div>
-                    <img src="Assets\img\css-letter-a.png" alt="Aspect Ratio">
-                    <button onclick="hideFeature()">Hide Result</button>
-                </div>`;
+        <div>
+            <img src="Assets/img/css-letter-a.png" alt="Aspect Ratio">
+            <button onclick="hideFeature(this)">Hide Result</button>
+        </div>`;
       break;
     case "B":
       content = `
-                <div>
-                    <img src="Assets\img\css-letter-b.png" alt="Border">
-                    <button onclick="hideFeature()">Hide Result</button>
-                </div>`;
+        <div>
+            <img src="Assets/img/css-letter-b.png" alt="Border">
+            <button onclick="hideFeature(this)">Hide Result</button>
+        </div>`;
       break;
     case "C":
       content = `
-                <div>
-                    <img src="Assets\img\css-letter-c.png" alt="Colors">
-                    <button onclick="hideFeature()">Hide Result</button>
-                </div>`;
-      break;
-    default:
+        <div>
+            <img src="Assets/img/css-letter-c.png" alt="Colors">
+            <button onclick="hideFeature(this)">Hide Result</button>
+        </div>`;
       break;
   }
 
-  displayArea.innerHTML = content;
+  displayArea.insertAdjacentHTML("beforeend", content);
 }
 
-function hideFeature() {
-  const displayArea = document.getElementById("display-area");
-  displayArea.innerHTML = "";
+function hideFeature(button) {
+  button.parentElement.remove();
 }
